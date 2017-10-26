e5fe734fc7127950f1209f139cf6277a6332bb2c
Tomasz Sztokinier <tomasz@todoit.pl>
2017-10-26 21:15:59 +0200

    manual tests using F# and C# that uses F# lib as proxy to access generated version string

---
+++ GitVersionTypeProvider.Tests.CS/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:AssemblyInformationalVersion(GitVersionTypeProvider.Tests.AssemblyInfo.Version)]
+
+namespace GitVersionTypeProvider.Tests.CS
+{
+    public class Tests
+    {
+        //GitVersionTypeProvider.Tests.AssemblyInfo
+        const string Version = GitVersionTypeProvider.Tests.AssemblyInfo.Version;
+    }
+}
---
+++ GitVersionTypeProvider.Tests/AssemblyInfo.fs
@@ -0,0 +1,9 @@
+﻿module GitVersionTypeProvider.Tests.AssemblyInfo
+
+open System.Reflection
+
+[<Literal>]
+let Version = Git.Version.Value
+
+[<assembly:AssemblyInformationalVersion(Git.Version.Value)>]
+do ()
